9fafad6b7ff1532e50304b697831fa1db97387d5
BrentGruber <brent.gruber77@gmail.com>
2021-10-25 16:44:52 -0400

    updating elapsed time to ms

--- services/climeat/app/main.py
+++ services/climeat/app/main.py
@@ -224,6 +224,7 @@ async def add_process_time_header(request: Request, call_next):
     start_time = time.time()
     response = await call_next(request)
     process_time = time.time() - start_time
+    process_time = process_time * 1000 # convert to ms
     response.headers["X-Process-Time"] = str(process_time)
     print(f"elapsed_time={process_time}")
     return response
